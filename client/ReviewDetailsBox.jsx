842cb5de37a80babfc0e62427c47d9f5821a61f6
Luke Brennan <LHB@Lukes-MacBook-Air-2.local>
2018-06-11 14:43:49 -0700

    convert review createdAt dates to long date format

--- client/ReviewDetailsBox.jsx
+++ client/ReviewDetailsBox.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+const createLongDate = function (datetime) {
+  const month = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const date = new Date(datetime.toLocaleString().split('T')[0]);
+  return `${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+};
+
 const ReviewDetailsBox = props => (
   <div className="review-details-box">
-    <span className="created-at"> {props.review.createdAt.toLocaleString().split('T')[0]} </span>
+    <span className="created-at"> {createLongDate(props.review.createdAt)} </span>
     <div className="title"> {props.review.title} </div>
     <div className="details"> {props.review.details} </div>
   </div>
